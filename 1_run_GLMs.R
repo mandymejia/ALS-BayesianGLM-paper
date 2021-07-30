6dd937ca6f58ed47c95999b05b3bf4dec28bf5c6
Amanda Mejia <mandy.mejia@gmail.com>
2021-07-30 12:17:29 -0400

    Update 1_run_GLMs.R

--- 1_run_GLMs.R
+++ 1_run_GLMs.R
@@ -1,18 +1,21 @@
 ######################################################
 # INSTALL AND LOAD DEPENDENCIES ----
 
+## fMRIscrub package
 #library(devtools)
 #install_github('mandymejia/fMRIscrub', ref='3.0') 
-#library(fMRIscrub) #clever()
+library(fMRIscrub) #clever()
 
-#CRAN packages --- install each with install.packages()
-library(gifti) #readgii
+## CRAN packages --- install each with install.packages()
+library(gifti) #readgii()
 library(ggplot2)
 library(ggthemes)
 
-## Install and load BayesfMRI package + ciftiTools + INLA dependencies
-
-#install_github('mandymejia/ciftiTools', ref='2.1') #1.2
+## BayesfMRI package + dependencies (ciftiTools + INLA + INLA dependencies)
+#library(devtools)
+#install_github('mandymejia/ciftiTools', ref='2.1')
+#install.packages('sp') #necessary INLA dependency
+#install.packages('foreach') #necessary INLA dependency
 #install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=FALSE)
 library(INLA)
 #inla.pardiso() #download PARDISO license for fast computation
@@ -28,11 +31,9 @@ main_dir <- '/my/project/dir' #path to repo contents
 data_dir <- file.path(main_dir,'data')
 result_dir <- file.path(main_dir,'results') 
 setwd(data_dir)
-
 load(file='subjects.Rdata') #subjects
 groups <- ifelse(grepl('A', subjects), 'ALS', 'HC')
 table(groups)
-
 task <- 'RRC' #analyze "real" right hand clenching task
 
 #read in design and 1st derivative
@@ -60,50 +61,42 @@ U <- length(thresholds)
 
 s <- 'C07' # example subject data in repo
 #for(s in subjects){
-  print(s)
   isubj <- which(subjects==s)
   setwd(file.path(data_dir,s))
 
   ### GET LIST OF VISITS
   
-  # files_s <- list.files(pattern = paste('*',task,'lh','fmri.csv',sep='_'))
-  # visits_s <- sort(substr(files_s, start=5, stop=6)) #get visit numbers
-  # #if(s=='C09') visits_s <- setdiff(visits_s,'08') #exclude due to bad registration
-  # #limit max number of visits to analyze
-  # if(length(visits_s) > 10){
-  #   which.visits <- round(seq(1, length(visits_s), length.out=10)) #roughly equally spaced, including first and last visit
-  #   visits_s <- visits_s[which.visits]
-  # }
-  # n_visits <- length(visits_s)
-  # n_visits_all[isubj] <- n_visits
-  # 
-  # 
-  # exclude <- NULL #any visits to exclude due to excessive outliers
-  # out_s <- vector('list', length=n_visits)
-  # for(v in 1:n_visits){
-  # 
-  #   print(paste0('visit ',v,' of ',n_visits))
-  # 
-  #   ### QUALITY CONTROL
-  # 
-  #   setwd(file.path(data_dir,s))
-  #   fname_BOLD_sv_lh <- paste(s,visits_s[v],task,'lh','fmri.csv',sep='_')
-  #   fname_BOLD_sv_rh <- paste(s,visits_s[v],task,'rh','fmri.csv',sep='_')
-  #   if(!(fname_BOLD_sv_lh %in% list.files())) break
-  #   if(!(fname_BOLD_sv_rh %in% list.files())) break
-  #   BOLD_sv_lh <- read.csv(fname_BOLD_sv_lh, header=FALSE)[,1:136]
-  #   BOLD_sv_rh <- read.csv(fname_BOLD_sv_rh, header=FALSE)[,1:136]
-  #   BOLD_sv <- rbind(BOLD_sv_lh, BOLD_sv_rh)
-  # 
-  #   #compute and plot leverage
-  #   PClev_sv <- clever(t(BOLD_sv))
-  #   out_s[[v]] <- PClev_sv$outlier_flags$leverage__PCA_kurt
-  #   pct_out <- mean(out_s[[v]])
-  #   if(pct_out > 0.25){ exclude <- c(exclude, v); next } #if more than 1/4 outliers, exclude visit (after loop)
-  # }
-  # 
-  #save(visits_s, out_s, exclude, file=paste0('visits_',s,'.RData'))
-  load(file=paste0('visits_',s,'.RData'))
+  files_s <- list.files(pattern = paste('*',task,'lh','fmri.csv',sep='_'))
+  visits_s <- sort(substr(files_s, start=5, stop=6)) #get visit numbers
+  #limit max number of visits to analyze
+  if(length(visits_s) > 10){
+    which.visits <- round(seq(1, length(visits_s), length.out=10)) #roughly equally spaced, including first and last visit
+    visits_s <- visits_s[which.visits]
+  }
+  n_visits <- length(visits_s)
+  #n_visits_all[isubj] <- n_visits
+
+  exclude <- NULL #any visits to exclude due to excessive outliers
+  out_s <- vector('list', length=n_visits)
+  for(v in 1:n_visits){
+    #read in data
+    setwd(file.path(data_dir,s))
+    fname_BOLD_sv_lh <- paste(s,visits_s[v],task,'lh','fmri.csv',sep='_')
+    fname_BOLD_sv_rh <- paste(s,visits_s[v],task,'rh','fmri.csv',sep='_')
+    if(!(fname_BOLD_sv_lh %in% list.files())) break
+    if(!(fname_BOLD_sv_rh %in% list.files())) break
+    BOLD_sv_lh <- read.csv(fname_BOLD_sv_lh, header=FALSE)[,1:136]
+    BOLD_sv_rh <- read.csv(fname_BOLD_sv_rh, header=FALSE)[,1:136]
+    BOLD_sv <- rbind(BOLD_sv_lh, BOLD_sv_rh)
+
+    #compute PCA leverage for scrubbing and QC
+    PClev_sv <- clever(t(BOLD_sv), measures='leverage')
+    out_s[[v]] <- PClev_sv$outlier_flags$leverage__PCA_kurt
+    pct_out <- mean(out_s[[v]])
+    if(pct_out > 0.25){ exclude <- c(exclude, v); next } #if more than 1/4 outliers, exclude visit (after loop)
+  }
+  save(visits_s, out_s, exclude, file=paste0('visits_',s,'.RData'))
+  #load(file=paste0('visits_',s,'.RData'))
   n_visits <- length(visits_s)
   #n_visits_all[isubj] <- n_visits
 
@@ -149,7 +142,7 @@ s <- 'C07' # example subject data in repo
       #create nuisance regressors & remove outliers
       fname_motion_sv <- paste(s,visits_s[[v]],task,'motion.txt',sep='_')
       motion_sv <- as.matrix(read.table(fname_motion_sv, header=FALSE))
-      dmotion_sv <- gradient(motion_sv)[1:136,] #remove extra volumes collected for one subject
+      dmotion_sv <- apply(motion_sv, 2, gradient)[1:136,] #remove extra volumes collected for one subject
       motion_sv <- motion_sv[1:136,] #remove extra volumes collected for one subject
       trend <- (1:136)/136 #for linear and quadratic time trends
       Z <- cbind(motion_sv, dmotion_sv, trend, trend^2)
@@ -177,7 +170,6 @@ s <- 'C07' # example subject data in repo
                                  num.threads = 4,
                                  avg_sessions = FALSE,
                                  scale_design=FALSE))
-    save(results_s, results0_s, file=file.path(result_dir, paste0('GLMs_',s,'.RData')))
 
     #comptime_df_ii <- data.frame(subject=s, hemisphere=hem, num_visits=n_visits, comptime=tmp[3])  #elapsed time (sec)
     #comptime_df <- rbind(comptime_df, comptime_df_ii)
@@ -241,6 +233,9 @@ s <- 'C07' # example subject data in repo
     #save(active_areas_df, comptime_act_df, file=file.path(result_dir,'activeareas_df.Rdata'))
  
   } #loop over hemispheres
+  
+  save(results0_s, file=file.path(result_dir, paste0('GLM_classical_',s,'.RData')))
+  save(results_s, file=file.path(result_dir, paste0('GLM_Bayesian_',s,'.RData')))
 #}
 
 # sum(n_visits_all[groups=='ALS'])
